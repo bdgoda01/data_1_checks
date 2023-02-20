d67bc519c39954532f762a6db22a6d54c9899f0a
bdgoda01 <binugodage@gmail.com>
2023-02-19 22:32:10 -0500

    data check 1

---
+++ datacheck.py
@@ -0,0 +1,17 @@
+#a statement that prints "hello world!"
+print("Hello World")
+
+#a list populated with several values. They can be strings / integers / floats etc. 
+#then, print() one of those values either to the prompt or in a cell (if using Jupyter Notebook)
+kpopGroups = ["NCT 127", "BTS", "TWICE", "Blackpink"]
+print(kpopGroups[1])
+
+#a dictionary populated with two keys and two values. 
+#print one of the values just like you did above.
+kpopAlbum = {"Album Name": "Between 1&2", "Artist Name": "TWICE"}
+print(kpopAlbum["Album Name"])
+
+#a tuple with 4 values. 
+#print one of them.
+blackpinkMembers = ("Rose", "Jennie", "Lisa", "Jisoo")
+print(blackpinkMembers[2])
